69bd64e878e8889a20e2f825fe66d6e8d2105ee0
Prashanth-Reddy19 <kotha.prashanth02>
2023-01-16 21:25:59 +0530

    Request Response

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const http=require('http');
 
 const server=http.createServer((req,res) => {
     console.log(req);
+    res.setHeader('Content-type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title><head>');
+    res.write('<body><h1>Hello my first Node.js Server</h1><body>');
+    res.write('</html>');
+    res.end();
     
-})
+});
 
-server.listen(4000);
+server.listen(3000);
